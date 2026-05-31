6cb31a0f8a8cf5c91e8160814b2e12db9c42f8bd
Elexvx Inc <62155410+johntao2004@users.noreply.github.com>
2026-05-31 10:42:34 +0800

    Add @ConfigurationPropertiesScan to register all sub-module @ConfigurationProperties beans

--- services/legendary-server/src/main/java/com/legendary/invention/server/LegendaryServerApplication.java
+++ services/legendary-server/src/main/java/com/legendary/invention/server/LegendaryServerApplication.java
@@ -2,10 +2,12 @@ package com.legendary.invention.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.apache.ibatis.annotations.Mapper;
 
 @SpringBootApplication(scanBasePackages = {"com.legendary.invention"})
+@ConfigurationPropertiesScan("com.legendary.invention")
 @MapperScan(basePackages = "com.legendary.invention", annotationClass = Mapper.class)
 public class LegendaryServerApplication {
 
@@ -13,3 +15,4 @@ public class LegendaryServerApplication {
         SpringApplication.run(LegendaryServerApplication.class, args);
     }
 }
+
