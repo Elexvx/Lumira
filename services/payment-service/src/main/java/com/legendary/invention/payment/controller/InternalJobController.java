109a8c14863adb93744c78404446835d501ae2a8
Elexvx Inc <62155410+johntao2004@users.noreply.github.com>
2026-06-08 22:06:36 +0800

    fix: resolve payment job controller bean clash

--- services/payment-service/src/main/java/com/legendary/invention/payment/controller/InternalJobController.java
+++ services/payment-service/src/main/java/com/legendary/invention/payment/controller/InternalJobController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("paymentInternalJobController")
 @RequestMapping("/internal/jobs")
 public class InternalJobController {
 
